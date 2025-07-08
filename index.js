deca6ae19cdc5b3b84128629512b1d47c146d080
payelray636 <payelray636@gmail.com>
2025-07-08 16:44:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
